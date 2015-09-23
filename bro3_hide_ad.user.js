272f4b9707e1e96387ae09dfd63673818a190152
soiyawalker <soiya.bro3@gmail.com>
2015-09-23 02:45:26 -0400

    clean: add comments

--- bro3_hide_ad.user.js
+++ bro3_hide_ad.user.js
@@ -3,20 +3,23 @@
 // @namespace	soiyawalker
 // @description	ブラウザ三国志 広告非表示ツール
 // @include		http://*.3gokushi.jp/*
-// @version		1.00
+// @version		1.0.1
 // @require		http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
 // @icon		http://pbs.twimg.com/profile_images/526631907543375872/eSaF5cj8.jpeg
 // ==/UserScript==
 
 // version //
-// 1.0 とりあえず広告削除
+// 1.0.0 とりあえず広告削除
+// 1.0.1 コメントの追加
 
 (function($) {
-  var advId = "#banner_set";
-  var newsId = "#whiteWrapper > div:nth-child(2) > iframe:nth-child(1)";
-  
-  $(advId).css("visibility", "hidden");
-  $(advId).css("height", "0px");
-  $(newsId).css("visibility", "hidden");
+    // ページ上部のバナー
+    var advId = "#banner_set";
 
+    // ページ下部のニュース
+    var newsId = "#whiteWrapper > div:nth-child(2) > iframe:nth-child(1)";
+  
+    $(advId).css("visibility", "hidden");
+    $(advId).css("height", "0px");
+    $(newsId).css("visibility", "hidden");
 })(jQuery);
